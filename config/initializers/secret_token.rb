b50658dbe24b43763524a12171fdf2545a6128d3
Brian McQuay <brian@onomojo.com>
2013-02-19 15:29:52 -0600

    initial rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rentals::Application.config.secret_key_base = 'a1211b1448dedb46f86201fb38028430144e6fe7abcd62ae431d8cfc5e04df851113499ac366fd76c77c2de65e3c91150e0e994c005221d602407556d4945d24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rentals::Application.config.session_store :encrypted_cookie_store, key: '_rentals_session'
