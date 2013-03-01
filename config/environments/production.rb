2d13251c5434dd90a67b1adcbeadb0a06ee4d98b
Brian McQuay <brian@onomojo.com>
2013-03-01 08:04:56 -0600

    trying to get production rails_admin assets working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rentals::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -57,6 +57,7 @@ Rentals::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.assets.initialize_on_precompile = false
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
