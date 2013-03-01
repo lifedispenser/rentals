719ed04071d95d71b74da048673e6d7b88fa88c0
Brian McQuay <brian@onomojo.com>
2013-03-01 08:29:18 -0600

    manually adding rails_admin assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rentals::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -57,7 +57,6 @@ Rentals::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  config.assets.initialize_on_precompile = false
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
