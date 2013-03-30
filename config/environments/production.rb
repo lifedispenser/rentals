407ba8db48446bc257ae2a3ebb7f09fa03c82865
Brian McQuay <brian@onomojo.com>
2013-03-29 18:37:52 -0600

    working on fixing production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rentals::Application.configure do
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  
   # Generate digests for assets URLs.
   config.assets.digest = true
 #  config.assets.initialize_on_precompile = false
